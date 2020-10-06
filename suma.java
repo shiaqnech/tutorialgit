82b90851d5db164bb99d5b3c96c9cb480134577d
shiaq <shiaq.gutierrez@cirvianum.cat>
2020-10-06 14:23:47 +0200

    -afitxer suma

---
+++ suma.java
@@ -0,0 +1,9 @@
+
+int numero1 = 0;
+int numero2 = 0:
+
+system.out.println("Introduce el primer numero: ");
+numero1 = reader.nextInt();
+
+System.out.println("Introduce el segundo número: ");
+numero2 = reader.nextInt();
